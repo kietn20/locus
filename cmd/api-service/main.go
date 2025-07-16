4f752c40f0fd4048ac5d34be797ef310c262a9b6
kietn20 <kietnguyen3698@gmail.com>
2025-07-15 17:35:02 -0700

    Refactor main function to improve server initialization and error handling

--- cmd/api-service/main.go
+++ cmd/api-service/main.go
@@ -7,12 +7,12 @@ import (
 	"log"
 	"net/http"
 
-	// "github.com/kietn20/locus/internal/db"
+	"github.com/kietn20/locus/internal/db"
 	"github.com/kietn20/locus/internal/geofence"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/jackc/pgx/v5"
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 type APIServer struct {
@@ -46,23 +46,22 @@ func (s *APIServer) setupRoutes() {
 
 
 func main() {
-	// Initialize the router and middleware 
-	// chi is a lightweight, idiomatic and composable router for building Go HTTP services
-	r := chi.NewRouter()
-	r.Use(middleware.Logger) // for logging HTTP requests
-	r.Use(middleware.Recoverer) // for recovering from panics and returning a 500 Internal Server Error
-	
-	// --- defining routes ---
-	r.Route("/api/v1", func(r chi.Router) {
-		r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("OK"))
-		})
-	})
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on external env vars")
+	}
 
-	// --- Start the HTTP server ---
+	// Connect to the database
+	conn := db.Connect()
+	defer conn.Close(context.Background())
+
+	// Run migrations (this will now create BOTH tables)
+	db.Migrate(conn)
+
+	// Create and start the server
+	server := NewAPIServer(conn)
 	port := "8080"
 	log.Printf("API server starting on port %s...", port)
-	if err := http.ListenAndServe(":"+port, r); err != nil {
+	if err := http.ListenAndServe(":"+port, server.Router); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
